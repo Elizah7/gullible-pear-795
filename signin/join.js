1c7b146491e558792e625e498aa7e543ab9f7352
vishalguptadishu <vishalguptadishu@gmail.com>
2022-08-27 20:50:26 +0530

    day-5

---
+++ signin/join.js
@@ -0,0 +1,41 @@
+
+
+function joinbtn(){
+    let email=document.querySelector("#email").value;
+    let password=document.querySelector("#password").value
+    let name=document.querySelector("#first_name").value 
+
+    function userdata(email,password,name){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+
+    }
+    
+
+    let s1=new userdata(email,password,name)
+    let isvalidated=false;
+   if(email==="")
+   {
+    alert("pleas enter email")
+   }
+
+   if(password===""  || password==="123")
+   {
+    alert("pleas enter right password")
+   }
+
+function datachek(){
+    let user=JSON.parse(localStorage.getItem("key"))|| []
+
+    user.push(s1)
+
+    localStorage.setItem("key",JSON.stringify(user))
+
+    alert("walcoome to asos")
+   
+    window.location.href="gullible-pear-795/signin/signin.html"
+}
+datachek()
+
+}
