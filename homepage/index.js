bae3fdd74980382bb633ce94537b73b77d7db515
aspiringsanjay <minebe83@gmail.com>
2022-08-27 23:36:11 +0530

    daychabge

--- homepage/index.js
+++ homepage/index.js
@@ -7,7 +7,40 @@ let linkfunction=()=>{
     window.location.href="../asos/women.html"
 }
 
-let btn=document.getElementById("linpage")
+let btn=document.getElementById("whitegap")
 btn.addEventListener("click",()=>{
     linkfunction()
 })
+let btn1=document.getElementById("linkpage")
+btn1.addEventListener("click",()=>{
+    linkfunction()
+})
+let btn2=document.getElementById("linkpage1")
+btn2.addEventListener("click",()=>{
+    linkfunction()
+})
+let btn3=document.getElementById("linkpage2")
+btn3.addEventListener("click",()=>{
+    linkfunction()
+})
+let btn4=document.getElementById("linkpage3")
+btn4.addEventListener("click",()=>{
+    linkfunction()
+})
+let btn5=document.getElementById("linkpage4")
+btn5.addEventListener("click",()=>{
+    linkfunction()
+})
+let btn6=document.getElementById("linkpage5")
+btn6.addEventListener("click",()=>{
+    linkfunction()
+})
+
+let loginfunction=()=>{
+    window.location.href="../signin/signin.html"
+}
+let loginlogo=document.getElementById("loginlogo")
+loginlogo.addEventListener("click",()=>{
+    loginfunction()
+})
+
